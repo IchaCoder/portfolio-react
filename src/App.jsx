b625b5ce725709028c4c46a80cb1bfeb22ba0be6
Emmanuel Yeboah <ichabordyeboah247@gmail.com>
2022-10-25 11:40:05 +0000

    fixing deployment bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import Navbar from "./component/nav/Navbar";
 import Home from "./Pages/home/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { SkeletonTheme } from "react-loading-skeleton";
 import About from "./Pages/about/About";
 import Portfolio from "./Pages/portfolio/Portfolio";
 import News from "./Pages/news/News";
@@ -10,20 +9,18 @@ import Contact from "./Pages/contact/Contact";
 
 function App() {
 	return (
-		<SkeletonTheme baseColor="#5294e0" highlightColor="#96c7ff">
-			<Router>
-				<main className="lg:flex ">
-					<Navbar />
-					<Routes>
-						<Route exact path="/" element={<Home />} />
-						<Route path="/about" element={<About />} />
-						<Route path="/portfolio" element={<Portfolio />} />
-						<Route path="/news" element={<News />} />
-						<Route path="/contact" element={<Contact />} />
-					</Routes>
-				</main>
-			</Router>
-		</SkeletonTheme>
+		<Router>
+			<main className="lg:flex ">
+				<Navbar />
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/portfolio" element={<Portfolio />} />
+					<Route path="/news" element={<News />} />
+					<Route path="/contact" element={<Contact />} />
+				</Routes>
+			</main>
+		</Router>
 	);
 }
 
